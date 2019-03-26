4f1103559021860fe8ac99c4943da70e1029ea88
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-26 12:21:45 +0100

    funciones

--- main.js
+++ main.js
@@ -1,36 +1,9 @@
-var peso = 63
-console.log('Peso: ' + peso)
+var nombre = 'Manuel', edad = 21
 
-var correr = 3
+function imprimirEdad(nombre, edad){
+    console.log(`${nombre} tiene ${edad} años.`)
+}
 
-peso -= correr
-
-console.log('Después de correr: ' + peso)
-
-var pastel = 5
-
-peso += pastel
-
-console.log('Después de pastel:' + peso)
-
-peso++; // Suma uno
-peso--; // Resta uno
-
-console.log(peso)
-
-var precioVino = 200.3
-
-//var subirPrecioVino = precioVino * 100 * 3 / 100 //Para que se me muestren los decimales justos
-var subirPrecioVino = Math.round(precioVino * 100 * 3) / 100
-var total = subirPrecioVino.toFixed(2)
-
-console.log(subirPrecioVino)
-
-console.log(total)
-
-var totalNumero = parseFloat(total)
-
-var porcionesPizza = 8
-var personas = 2
-
-console.log(porcionesPizza / personas)
+imprimirEdad(nombre, edad)
+imprimirEdad('Pedro', 17)
+imprimirEdad('Pedro')

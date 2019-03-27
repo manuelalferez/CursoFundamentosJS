14b77e19e8c6be290e884491a7fecc33119b82ac
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-27 11:44:22 +0100

    clases en JS

--- main.js
+++ main.js
@@ -1,49 +1,14 @@
-var sacha = {
-    nombre: 'Sacha',
-    apellido: 'Lifszyc',
-    altura: 1.72,
-    cantidadDeLibros: 111
+function Persona(nombre, apellido, edad) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.edad = edad
 }
 
-var alan = {
-    nombre: 'Alan',
-    apellido: 'Perez',
-    altura: 1.86,
-    cantidadDeLibros: 78
-}
+//Hay que especificar que es una función, no sirve una arrow function
+Persona.prototype.saludar = function () { console.log(`Hola soy ${this.nombre} ${this.apellido} y tengo ${this.edad}`) }
 
-var martin = {
-    nombre: 'Martin',
-    apellido: 'Gomez',
-    altura: 1.85,
-    cantidadDeLibros: 132
-}
+var persona = new Persona('Manuel', 'Alférez', 21)
+var fran = new Persona('Francisco', 'Ruiz', 48)
 
-var dario = {
-    nombre: 'Dario',
-    apellido: 'Juarez',
-    altura: 1.71,
-    cantidadDeLibros: 90
-}
-
-var vicky = {
-    nombre: 'Vicky',
-    apellido: 'Zapata',
-    altura: 1.56,
-    cantidadDeLibros: 91
-}
-
-var paula = {
-    nombre: 'Paula',
-    apellido: 'Barros',
-    altura: 1.76,
-    cantidadDeLibros: 182
-}
-
-var personas = [sacha, alan, martin, dario, vicky, paula] // Array
-
-reducer = (acum, { cantidadDeLibros }) => acum += cantidadDeLibros
-
-var totalLibros = personas.reduce(reducer, 0) //Primero la función con las operaciones y después el valor inicial del contador
-
-console.table(`Se han leído ${totalLibros} libros`) // Permite verlo en forma de tabla
+persona.saludar()
+fran.saludar()

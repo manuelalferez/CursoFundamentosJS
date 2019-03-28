775bc73c69830060cb2860dd698622271f6a1803
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-28 10:00:39 +0100

    comenzando el juego: creacion de clase Juego y funcionalidad de click sobre el boton

--- main.js
+++
@@ -1,5 +0,0 @@
-console.log('a')
-setTimeout( () => console.log('b'), 2000) 
-console.log('c')
-setTimeout( () => console.log('d'), 1000) 
-// Las añade a una cola distinta, y las sacará cuando termine la ejecucion del programa principal 

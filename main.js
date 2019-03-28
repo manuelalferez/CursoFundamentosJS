6c3e691f46f3a1063eee16a1dfe775d052a6955e
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-28 07:55:14 +0100

    herencia

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function heredaDe (hijo, padre){
+    var fn = function () {}
+    fn.prototype = padre.prototype
+    hijo.prototype = new fn
+    hijo.prototype.constructor = hijo
+}
+
 function Persona(nombre, apellido, edad) {
     this.nombre = nombre
     this.apellido = apellido
@@ -7,8 +14,17 @@ function Persona(nombre, apellido, edad) {
 //Hay que especificar que es una función, no sirve una arrow function
 Persona.prototype.saludar = function () { console.log(`Hola soy ${this.nombre} ${this.apellido} y tengo ${this.edad}`) }
 
+function Desarrollador (nombre, apellido){
+    this.nombre = nombre
+    this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () { console.log(`Hola soy ${this.nombre} ${this.apellido} y soy desarrollador`)}
+
 var persona = new Persona('Manuel', 'Alférez', 21)
-var fran = new Persona('Francisco', 'Ruiz', 48)
+var fran = new Desarrollador('Francisco', 'Ruiz', 48)
 
 persona.saludar()
 fran.saludar()

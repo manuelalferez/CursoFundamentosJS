569f2ae0db36454d324096b0b538402764775cfc
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-28 08:09:28 +0100

    clases en JS

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-function heredaDe (hijo, padre){
-    var fn = function () {}
-    fn.prototype = padre.prototype
-    hijo.prototype = new fn
-    hijo.prototype.constructor = hijo
+class Persona {
+    constructor(nombre, apellido, edad) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+    }
+    //No es necesario usar function 
+    saludar() {
+        console.log(`Hola soy ${this.nombre} ${this.apellido} y tengo ${this.edad}`)
+    }
+    soyMayor = () => this.edad > 18
 }
 
-function Persona(nombre, apellido, edad) {
-    this.nombre = nombre
-    this.apellido = apellido
-    this.edad = edad
+class Desarrollador extends Persona {
+    constructor(nombre, apellido, edad) {
+        super(nombre, apellido, edad)
+    }
+
+    saludar() {
+        console.log(`Hola soy ${this.nombre} ${this.apellido} y soy desarrollador`)
+    }
+    soyMenor = () => this.edad < 18
 }
-
-//Hay que especificar que es una función, no sirve una arrow function
-Persona.prototype.saludar = function () { console.log(`Hola soy ${this.nombre} ${this.apellido} y tengo ${this.edad}`) }
-
-function Desarrollador (nombre, apellido){
-    this.nombre = nombre
-    this.apellido = apellido
-}
-
-heredaDe(Desarrollador, Persona)
-
-Desarrollador.prototype.saludar = function () { console.log(`Hola soy ${this.nombre} ${this.apellido} y soy desarrollador`)}
 
 var persona = new Persona('Manuel', 'Alférez', 21)
 var fran = new Desarrollador('Francisco', 'Ruiz', 48)
